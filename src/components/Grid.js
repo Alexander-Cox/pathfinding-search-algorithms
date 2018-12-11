df744964369cbb6e2002bf1b9b9d1b5a962fcea2
Alexander Cox <alexandercox@Alexs-NC-MacBook.local>
2018-12-11 09:51:05 +0000

    add wall functionality

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import Spot from "./Spot";
 import './styles/Grid.css';
+import { wallsCoords } from '../data'
 
 class Grid extends Component {
+
   state = {
-    grid: Array.from({ length: 10 }, (_, i) => {
-      return Array.from({ length: 10 }, (_, j) => {
-        return { f: Infinity, g: Infinity };
+    grid: Array.from({ length: 10 }, _ => {
+      return Array.from({ length: 10 }, _ => {
+        return { f: Infinity, g: Infinity, isPath: false, isWall: false };
       });
     })
-  };
+  }
+
+  createNewGridFromState(grid) {
+    return grid.map(col => {
+      return col.map(spotProps => {
+        return { ...spotProps };
+      });
+    })
+  }
+
+  componentDidMount = () => {
+    this.setState(({ grid }) => {
+      const newGrid = this.createNewGridFromState(grid);
+      console.log(wallsCoords)
+      for (let i = 0; i < wallsCoords.length; i += 2) {
+        const x = wallsCoords[i];
+        const y = wallsCoords[i + 1];
+        newGrid[x][y].isWall = true;
+      }
+      return { grid: newGrid };
+    })
+  }
+
   render() {
     const { grid } = this.state;
     return (
@@ -22,6 +46,7 @@ class Grid extends Component {
       </div>
     );
   }
+
 }
 
 export default Grid;
--- src/components/Spot.js
+++ src/components/Spot.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './styles/Spot.css'
 
 const Spot = (props) => {
-    const { i, j } = props;
+    const { i, j, isWall, isPath } = props;
     function createStyles(i, j) {
-        return {
+        const styles = {
             gridColumnStart: i + 1,
-                gridRowStart: j + 1
-              }
+            gridRowStart: j + 1
+        }
+        if (isWall) styles.backgroundColor = "Black";
+        return styles;
     }
-    return ( 
-        <div className="spot" style={createStyles(i,j)}>
-            
+    return (
+        <div className="spot" style={createStyles(i, j)}>
+
         </div>
-     );
+    );
 }
- 
+
 export default Spot;
